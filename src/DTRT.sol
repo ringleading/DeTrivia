5e89c5d1cc04088c0b13fbb51418065d06414b78
lingreading <wouldlikesomebanter@gmail.com>
2024-11-22 15:27:41 +0800

    Change to RBAC instead of Ownable

--- src/DTRT.sol
+++ src/DTRT.sol
@@ -5,43 +5,46 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract DeTriviaRewardToken is
     ERC20,
     ERC20Burnable,
     ERC20Pausable,
-    Ownable,
+    AccessControl,
     ERC20Permit
 {
-    constructor(
-        address initialOwner
-    )
-        ERC20("DeTriviaRewardToken", "DTRT")
-        Ownable(initialOwner)
-        ERC20Permit("DeTriviaRewardToken")
-    {}
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    function pause() public onlyOwner {
+    constructor(address initialOwner)
+        ERC20("DeTriviaRewardToken", "DTRT")
+        ERC20Permit("DeTriviaRewardToken")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+        _grantRole(MINTER_ROLE, initialOwner);
+        _grantRole(PAUSER_ROLE, initialOwner);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal override(ERC20, ERC20Pausable) {
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
         super._update(from, to, value);
     }
 }
